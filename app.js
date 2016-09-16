5393efb0bcfbcd7a1a1a345e719376b4e11a15e2
Luke Karrys <lukekarrys@gmail.com>
2016-09-16 01:30:23 -0700

    kafka offsets and snapshots

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const _ = require('lodash')
 const path = require('path')
 const server = require('http').createServer()
 const WebSocketServer = require('ws').Server
@@ -15,8 +16,9 @@ const app = express()
 app.use(express.static(path.join(__dirname, 'dist')))
 
 if (process.env.NODE_ENV !== 'production') {
+  const compiler = webpack(require('./webpack.config'))
   app.use(require('connect-history-api-fallback')({ verbose: false }))
-  app.use(require('webpack-dev-middleware')(webpack(require('./webpack.config'))))
+  app.use(require('webpack-dev-middleware')(compiler, { quiet: true, noInfo: true }))
 } else {
   app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
   app.get('/heroku', (req, res) => res.sendFile(path.join(__dirname, 'dist/index.html')))
@@ -34,16 +36,27 @@ server.listen(port, () => console.log(`http/ws server listening on ${port}`))
 const wss = new WebSocketServer({ server: server })
 const send = (data) => (client) => client.send(JSON.stringify(data))
 const broadcast = (data) => wss.clients.forEach(send(data))
+wss.on('connection', (ws) => send({ type: 'snapshot', data: snapshot })(ws))
+
+/*
+ * Configure topics and snapshot to store rolling data window
+ *
+ */
+const ROLLING_WINDOW = 30 * 60
+const topics = [
+  { topic: 'news-aggregate', offset: ROLLING_WINDOW },
+  { topic: 'music-aggregate', offset: ROLLING_WINDOW },
+  { topic: 'news-relatedwords' },
+  { topic: 'music-relatedwords' }
+]
+const snapshot = _.transform(topics, (res, topic) => { res[topic.topic] = null }, {})
 
 /*
  * Configure Kafka consumer
  *
  */
-const topics = ['news-aggregate', 'music-aggregate', 'news-relatedwords', 'music-relatedwords']
-
 const consumer = new Kafka.SimpleConsumer({
   idleTimeout: 100,
-  clientId: 'simple-consumer',
   connectionString: process.env.KAFKA_URL.replace(/\+ssl/g, ''),
   ssl: {
     certFile: './client.crt',
@@ -51,16 +64,40 @@ const consumer = new Kafka.SimpleConsumer({
   }
 })
 
-/*
- * Init Kafka consumer and data handler
- *
- */
-const dataHandler = (subscription) => (messageSet) => messageSet.forEach((m) => {
-  const data = JSON.parse(m.message.value.toString('utf8'))
-  const [topic, type] = subscription.split('-')
-  broadcast({ topic, type, data })
+const subscribe = (topic, options = {}) => consumer.subscribe(topic, options, (messageSet, topic) => {
+  const [name, type] = topic.split('-')
+  const messages = messageSet.map((m) => JSON.parse(m.message.value.toString('utf8')))
+  const hasOffset = !!options.offset
+  const existing = snapshot[topic]
+
+  if (existing === null) {
+    if (hasOffset) {
+      snapshot[topic] = messages.slice(messages.length > ROLLING_WINDOW ? messages.length - ROLLING_WINDOW : 0)
+    } else {
+      snapshot[topic] = _.last(messages)
+    }
+  } else {
+    if (hasOffset) {
+      if (existing.length + messages.length >= ROLLING_WINDOW) {
+        snapshot[topic] = [...snapshot[topic].slice(existing.length + messages.length - ROLLING_WINDOW), ...messages]
+      } else {
+        snapshot[topic].push(messages)
+      }
+      messages.forEach((message) => broadcast({ name, type, data: message }))
+    } else {
+      const latest = _.last(messages)
+      snapshot[topic] = latest
+      broadcast({ name, type, data: latest })
+    }
+  }
 })
 
-const subscribe = (topic) => consumer.subscribe(topic, [0], dataHandler(topic))
-
-consumer.init().then(() => Promise.all(topics.map(subscribe)))
+consumer.init().then(() => {
+  topics.forEach((topic) => {
+    if (topic.offset) {
+      consumer.offset(topic.topic).then((offset) => subscribe(topic.topic, { offset: offset - topic.offset }))
+    } else {
+      subscribe(topic.topic)
+    }
+  })
+})

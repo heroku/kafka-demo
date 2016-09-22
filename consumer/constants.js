520ecd05ed2939c69c14edb89521767d321e407e
Luke Karrys <lukekarrys@gmail.com>
2016-09-22 13:56:22 -0700

    increase server side data buffer

--- consumer/constants.js
+++ consumer/constants.js
@@ -4,8 +4,8 @@
 const interval = module.exports.INTERVAL = 1000
 
 // Max items to display for rolling data
-// Use a 5% buffer to prevent transitions from being too small for the chart bounds
+// Use a 10% buffer to prevent transitions from being too small for the chart bounds
 const maxSize = module.exports.MAX_SIZE = 1 * 60 * (1000 / interval)
-module.exports.MAX_BUFFER_SIZE = Math.floor(maxSize * 1.05)
+module.exports.MAX_BUFFER_SIZE = Math.floor(maxSize * 1.10)
 
 module.exports.TOPICS = ['news', 'music']

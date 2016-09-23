f606bf9e6f04c5000012c7e57abc59b308e59c8f
Luke Karrys <lukekarrys@gmail.com>
2016-09-23 11:59:58 -0700

    define twitter terms for client build too

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const path = require('path')
+const webpack = require('webpack')
 const HtmlPlugin = require('html-webpack-plugin')
 const ExtractTextPlugin = require('extract-text-webpack-plugin')
 const CleanPlugin = require('clean-webpack-plugin')
@@ -54,6 +55,9 @@ module.exports = {
   plugins: [
     html('index.html', theme),
     new CleanPlugin(['dist'], { root: __dirname, verbose: false }),
+    new webpack.DefinePlugin({
+      'process.env': { TWITTER_TRACK_TERMS: JSON.stringify(process.env.TWITTER_TRACK_TERMS) }
+    }),
     production && new ExtractTextPlugin('app.[contenthash].css'),
     !production && new (require('webpack-livereload-plugin'))()
   ].filter(Boolean)

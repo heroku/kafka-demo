e52393b4a9cbba89ad6b8314f031c081610b30d8
Luke Karrys <lukekarrys@gmail.com>
2016-09-26 23:10:14 -0700

    improve memory usage for bubble chart

--- src/lib/bubbles.js
+++ src/lib/bubbles.js
@@ -6,6 +6,7 @@ import dateFormat from 'dateformat'
 
 const dataId = ({ name, topic }) => `${name}-${topic}`
 const tooltipHtml = ({ name, r }) => `${name}<br/>${r}`
+const CALC_FONT_SIZE = 12
 
 export default class BubblesChart {
   constructor (options) {
@@ -14,6 +15,7 @@ export default class BubblesChart {
     this.transition = options.transition / 2
     this.maxRelations = options.maxRelations
     this._useInitial = false
+    this._textWidths = {}
 
     const svg = d3
       .select(this.container)
@@ -39,7 +41,7 @@ export default class BubblesChart {
   }
 
   getClass (d) {
-    return `chart-color-${this._topics.indexOf(d.topic) + 1} ${d.name}`
+    return `chart-color-${this._topics.indexOf(d.topic) + 1}`
   }
 
   formatData (raw) {
@@ -102,140 +104,115 @@ export default class BubblesChart {
     const height = this.getHeight()
     const width = this.getWidth()
     const center = { x: width / 2, y: height / 2 }
-    const translate = `${center.x},${center.y}`
-    const scale = Math.min(height, width) / ((enclose.r || 1) * 2)
+    const scale = Math.min(height, width) / ((enclose.r * 2) || 1)
 
-    this._textWidths = this._textWidths || {}
-    const calcFontSize = 12
-    const fontSize = (d, index, nodes) => {
-      const size = ((d.r * 2) / this._textWidths[d.name]) * calcFontSize * 0.7
-      const relativeSize = size * scale
-      return relativeSize < 10 ? 0 : `${size}px`
-    }
-
+    const hideTooltip = () => this.tooltip.attr('id', null).style('opacity', 0)
     const showTooltip = (d) => {
       this.tooltip.html(tooltipHtml(d))
 
       const rect = this.container.getBoundingClientRect()
+      const node = this.tooltip.node()
 
       this.tooltip
         .attr('id', dataId(d))
-        .style('left', `${(d.x * scale) + (width / 2) - (this.tooltip.node().clientWidth / 2) + rect.left}px`)
+        .style('left', `${(d.x * scale) + (width / 2) - (node.clientWidth / 2) + rect.left}px`)
         .style('top', `${(d.y * scale) + (height / 2) + (d.r * scale) + 2 + rect.top}px`)
         .attr('class', `tooltip ${this.getClass(d)}`)
         .style('opacity', 1)
     }
 
-    const hideTooltip = () => {
-      this.tooltip
-        .attr('id', null)
-        .style('opacity', 0)
-    }
-
     // Update tooltip html if it is active
     const tid = this.tooltip.attr('id')
-    if (tid) {
-      this.tooltip.html(tooltipHtml(_.find(data, (d) => dataId(d) === tid)))
-    }
+    if (tid) this.tooltip.html(tooltipHtml(_.find(data, (d) => dataId(d) === tid)))
 
-    this.chartArea
-      .transition()
-      .duration(options.transition)
-      .ease(d3.easeLinear)
-      .attr('transform', `translate(${translate}) scale(${scale})`)
-
-    const containers = this.chartArea
+    const updateSelection = this.chartArea
       .selectAll('.circle-node')
       .data(data, dataId)
 
-    /*
-     * EXITING
-     */
-    const exiting = containers.exit()
-
-    exiting
-      .transition()
-      .duration(this.transition * 0.5)
-      .ease(d3.easeLinear)
-      .attr('transform', `translate(0,0)`)
-      .remove()
-
-    exiting
+    const updateCircles = updateSelection
       .select('circle')
+
+    const updateText = updateSelection
+      .select('text')
+
+    /*
+     * Setup initial properties on entering elements that wont change
+     * on new data
+     */
+    const enterSelection = updateSelection
+      .enter()
+      .append('g')
+      .attr('class', 'circle-node')
+      .style('text-anchor', 'middle')
+      .style('opacity', 0)
+      .attr('transform', `translate(${center.x}, ${center.y}) scale(0)`)
+      .on('mouseenter', showTooltip)
+      .on('mouseleave', hideTooltip)
+
+    const enterCircles = enterSelection
+      .append('circle')
+      .attr('class', (d) => this.getClass(d))
+      .attr('r', 0)
+
+    const enterText = enterSelection
+      .append('text')
+      .text((d) => d.name)
+      .style('text-anchor', 'middle')
+      .attr('fill', '#fff')
+      .attr('dy', '.35em')
+
+    /*
+     * Animate out and remove exiting data
+     */
+    updateSelection
+      .exit()
       .transition()
       .duration(this.transition * 0.5)
       .ease(d3.easeLinear)
       .style('opacity', 0)
-      .attr('r', 0)
-
-    exiting
-      .select('text')
+      .attr('transform', `translate(${center.x}, ${center.y}) scale(0)`)
       .remove()
 
     /*
-     * UPDATE EXISTING
+     * Keep a cache of all related words' computed text length
      */
-    containers
-      .on('mouseenter', showTooltip)
-      .on('mouseleave', hideTooltip)
-      .transition()
-      .duration(options.transition)
-      .ease(d3.easeLinear)
-      .attr('transform', (d) => `translate(${d.x},${d.y})`)
-
-    containers
-      .select('circle')
-      .transition()
-      .duration(options.transition)
-      .ease(d3.easeLinear)
-      .attr('r', (d) => d.r)
-
-    containers
-      .select('text')
-      .transition()
-      .duration(options.transition)
-      .ease(d3.easeLinear)
-      .style('font-size', fontSize)
-
-    /*
-     * ADD NEW
-     */
-    const enter = containers
-      .enter()
-      .append('g')
-
-    enter
-      .attr('class', 'circle-node')
-      .attr('transform', 'translate(0,0)')
-      .on('mouseenter', showTooltip)
-      .on('mouseleave', hideTooltip)
-      .transition()
-      .duration(options.transition)
-      .ease(d3.easeLinear)
-      .attr('transform', (d) => `translate(${d.x},${d.y})`)
-
-    enter
-      .append('circle')
-      .attr('class', (d) => this.getClass(d))
-      .attr('r', (d) => d.r)
-
-    // Keep a cache of all related words' computed text length
-    enter
+    enterSelection
       .filter((d) => !this._textWidths[d.name])
       .append('text')
-      .attr('font-size', `${calcFontSize}px`)
+      .attr('font-size', `${CALC_FONT_SIZE}px`)
       .text((d) => d.name)
       .each((d, i, nodes) => {
         this._textWidths[d.name] = nodes[i].getComputedTextLength()
         nodes[i].remove()
       })
 
-    enter
-      .append('text')
-      .text((d) => d.name)
-      .style('text-anchor', 'middle')
-      .attr('fill', '#fff')
-      .attr('dy', '.35em')
-      .style('font-size', fontSize)
+    /*
+     * Merge current and new elements are animate them the same
+     */
+    enterSelection
+      .merge(updateSelection)
+      .transition()
+      .duration(options.transition)
+      .ease(d3.easeLinear)
+      .style('opacity', 1)
+      .attr('transform', (d) => `translate(${(d.x * scale) + center.x},${(d.y * scale) + center.y}) scale(${scale})`)
+
+    enterCircles
+      .merge(updateCircles)
+      .transition()
+      .duration(options.transition)
+      .ease(d3.easeLinear)
+      .attr('r', (d) => d.r)
+
+    enterText
+      .merge(updateText)
+      .transition()
+      .duration(options.transition)
+      .ease(d3.easeLinear)
+      .style('font-size', (d, index, nodes) => {
+        const size = ((d.r * 2) / (this._textWidths[d.name] || 50)) * CALC_FONT_SIZE * 0.7
+        const relativeSize = size * scale
+        return relativeSize < 10 ? 0 : `${size}px`
+      })
   }
 }
